9597954200acb86edacfa67f4e81671247f7afe9
Artur Motin <artur.motin98@gmail.com>
2023-02-11 13:24:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.get('/api/:projectName', (request, response)=>{
     }
 })
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
